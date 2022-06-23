be2d76dacecf6d405bde5966aff98eb23b6e6fb0
Prototype_Nox <azemalexander@gmail.com>
2022-06-22 22:00:03 -0500

    1 commit vite

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <h2>Hey Vite How Are you?</h2>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
